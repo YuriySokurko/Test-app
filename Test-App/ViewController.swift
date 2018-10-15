e5b55f412091448aeecd4b796532913c66ec1c53
Yuriy Sokirko <y.sokirko@onlinepay.com>
2018-10-15 15:47:20 +0300

    initil commit

---
+++ Test-App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test-App
+//
+//  Created by Yuriy Sokirko on 10/15/18.
+//  Copyright © 2018 Yuriy Sokirko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
